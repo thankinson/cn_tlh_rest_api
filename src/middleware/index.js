953bf98f94e21501bd5328baf61225be98cf1c0a
thankinson <t_hankinson@outlook.com>
2022-03-08 16:18:04 +0000

    update

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const { jwt } = require("jsonwebtoken");
 const User = require("../user/userModel");
 
 exports.hashPassword = async (req, res, next) => {
@@ -13,14 +14,26 @@ exports.hashPassword = async (req, res, next) => {
 
 exports.decryptPassword = async (req, res, next) => {
     try {
-        req.user = await User.findOne({ username: req.body.username })
+        req.user = await User.findOne({ username: req.body.username });
         if (await bcrypt.compare(req.body.password, req.user.password)) {
             next();
         } else {
-            throw new Error("incorect credentials")
+            throw new Error("incorect credentials");
         }
     } catch (error) {
         console.log(error);
-        res.status(500).send({err: error.message})
+        res.status(500).send({err: error.message});
     }
 };
+
+exports.checkToken = async (req, res, next) => {
+    try {
+        const token = req.header("Authorization").replace("Bearer ", "");
+        const decodedToken = await jwt.verify(token, process.env.SECRET);
+        const user = await User.findById();
+        next();
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({err: error.message});
+    }
+};                        
--- src/user/userControlers.js
+++ src/user/userControlers.js
@@ -4,8 +4,8 @@ const User = require("./userModel");
 exports.addUser = async (req, res) => {
     try {
         const newUser = await User.create(req.body);
-        const token = await jwt.sign({_id: newUser._id}, process.env.SECRET)
-        res.status(200).send({user: newUser.username})
+        const token = await jwt.sign({_id: newUser._id}, process.env.SECRET);
+        res.status(200).send({user: newUser.username, token })
     } catch (error) {
         console.log(error);
         res.status(500).send({err: error.message})

435aba856c27774c6b2cd97d1982f25290429571
thankinson <t_hankinson@outlook.com>
2022-03-08 11:18:51 +0000

    update

---
+++ src/movie/movieControllers.js
@@ -0,0 +1,47 @@
+const Movie = require("./movieModel");
+
+exports.addMovie = async (req, res) =>{
+    try {
+        const newMovie = await Movie.create(req.body);
+        res.status(200).send({movie: newMovie});
+    } catch (error) {   
+        console.log(error);
+        res.status(500).send({err: error.message});
+    };
+};
+
+exports.listMovie = async (req, res) => {
+    try {
+        const movies = await Movie.find({})
+        res.status(200).send({allMovie: movies});
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({err: error.message});
+    };
+};
+
+exports.updateMovie = async (req, res) => {
+    try {
+        const movies = await Movie.updateOne(
+           { [req.param.filterKey]: req.param.filterVal },
+           { [req.param.updateKey]: req.param.updateVal } 
+        );
+        res.status(200).send({update: movies});
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({err: error.message});
+    };
+};
+
+exports.deleteMovie = async (req, res) => {
+    try {
+        const remove = await Movie.deleteOne(
+            //{title: req.body.title} this dose not work on full stack
+            {[req.param.filterKey]: req.param.deleteVal}
+            )
+        res.status(200).send({remove: remove})
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({err: error.message});
+    };
+};
--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 require("./db/connection");
 const exspress = require("express");
+const movieRouter = require("./movie/movieRoutes");
 const app = exspress();
 const port = 5000;
 
 app.use(exspress.json());
+app.use(movieRouter);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
-})
+});

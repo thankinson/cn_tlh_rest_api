f8f4c128deecf43042474b0bad31f1be8e883736
thankinson <t_hankinson@outlook.com>
2022-03-16 15:39:46 +0000

    fixed token issue

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcryptjs");
-const { jwt } = require("jsonwebtoken");
+const  jwt  = require("jsonwebtoken");
 const User = require("../user/userModel");
 
 exports.hashPassword = async (req, res, next) => {
